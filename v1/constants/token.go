5b4789e1e71c86fcbaeb38c902fcce8c3799cc4e
synycboom <synycboom@gmail.com>
2022-04-04 22:21:35 +0700

    feat: add algo token constants

---
+++ v1/constants/token.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	// AlgoTokenName is the algo token name
+	AlgoTokenName = "Algo"
+
+	// AlgoTokenUnitName is the algo token unit name
+	AlgoTokenUnitName = "ALGO"
+
+	// AlgoTokenDecimals is the algo token decimals
+	AlgoTokenDecimals = 6
+)
