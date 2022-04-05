e34e6d9e0f1874bedb6ab3bb7de74b8b4adfb586
synycboom <synycboom@gmail.com>
2022-04-05 12:37:49 +0700

    test: add tests for converter and state

---
+++ utils/converter_test.go
@@ -0,0 +1,32 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/synycboom/tinyman-go-sdk/utils"
+)
+
+func TestConverter(t *testing.T) {
+	var testConverter = func(val uint64, expected []byte) {
+		bb, err := utils.IntToBytes(val)
+		if err != nil {
+			t.Errorf("Unexpected error %s", err.Error())
+			return
+		}
+		if len(bb) != len(expected) {
+			t.Errorf("Converter returns wrong value length %v", len(bb))
+			return
+		}
+		for idx, b := range bb {
+			if b != expected[idx] {
+				t.Errorf("Converter returns wrong value %v at index %d", b, idx)
+			}
+		}
+	}
+
+	testConverter(0, []byte{0, 0, 0, 0, 0, 0, 0, 0})
+	testConverter(127, []byte{0, 0, 0, 0, 0, 0, 0, 127})
+	testConverter(128, []byte{0, 0, 0, 0, 0, 0, 0, 128})
+	testConverter(255, []byte{0, 0, 0, 0, 0, 0, 0, 255})
+	testConverter(512, []byte{0, 0, 0, 0, 0, 0, 2, 0})
+}
