8d57e3af01dad20c737bd03728cf37fc7faff587
synycboom <synycboom@gmail.com>
2022-04-05 13:50:23 +0700

    feat: implement pool swap

---
+++ v1/pools/swap.go
@@ -0,0 +1,66 @@
+package pools
+
+import (
+	"context"
+
+	"github.com/synycboom/tinyman-go-sdk/types"
+	"github.com/synycboom/tinyman-go-sdk/utils"
+	"github.com/synycboom/tinyman-go-sdk/v1/prepare"
+)
+
+// PrepareSwapTransactions prepares swap transaction and returns a transaction group
+func (p *Pool) PrepareSwapTransactions(
+	ctx context.Context,
+	assetAmountIn,
+	assetAmountOut types.AssetAmount,
+	swapType,
+	swapperAddress string,
+) (*utils.TransactionGroup, error) {
+	if len(swapperAddress) == 0 {
+		swapperAddress = p.UserAddress
+	}
+
+	sp, err := p.ac.SuggestedParams().Do(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	txGroup, err := prepare.SwapTransactions(
+		p.ValidatorAppID,
+		p.Asset1.ID,
+		p.Asset2.ID,
+		p.LiquidityAsset.ID,
+		assetAmountIn.Asset.ID,
+		assetAmountIn.Amount,
+		assetAmountOut.Amount,
+		swapType,
+		swapperAddress,
+		sp,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return txGroup, nil
+}
+
+// PrepareSwapTransactionsFromQuote prepares swap transaction from a given swap quote and returns a transaction group
+func (p *Pool) PrepareSwapTransactionsFromQuote(ctx context.Context, quote *types.SwapQuote, swapperAddress string) (*utils.TransactionGroup, error) {
+	amountOut, err := quote.AmountOutWithSlippage()
+	if err != nil {
+		return nil, err
+	}
+
+	txGroup, err := p.PrepareSwapTransactions(
+		ctx,
+		quote.AmountIn,
+		*amountOut,
+		quote.SwapType,
+		swapperAddress,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return txGroup, nil
+}
